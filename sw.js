bc826b66f1f7f1bfd6ed58a1dd47a7b9c51d10ee
yuicalendar2026 <haurusoul@gmail.com>
2026-03-04 18:37:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,9 @@ const CACHE_NAME = "kaiun-calendar-v2";
 const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
