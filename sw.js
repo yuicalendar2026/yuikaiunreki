6bcc2cc8c8523d1f7cc0ec4c54a2c73e9de399e7
yuicalendar2026 <haurusoul@gmail.com>
2026-03-04 17:39:12 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open("kaiun").then(cache=>{
+return cache.addAll(["./"]);
+})
+);
+});
 
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res||fetch(e.request);
+})
+);
+});
